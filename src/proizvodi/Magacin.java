327aad2061084df4d32cdf9bac8da7c3686a9e7f
doktoree <tjuring901@gmail.com>
2024-03-04 15:36:40 +0100

    Dodata klasa Magacin koja implementira interfejs

---
+++ src/proizvodi/Magacin.java
@@ -0,0 +1,27 @@
+package proizvodi;
+
+import java.util.List;
+
+import interfejs.SpecifikacijaInterfejs;
+
+public class Magacin implements SpecifikacijaInterfejs {
+
+	@Override
+	public void dodajArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Artikal> vratiArtikle(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
