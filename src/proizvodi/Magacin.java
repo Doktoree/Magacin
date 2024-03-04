3a83576ee131413db38665723be7b3fd60d03058
doktoree <tjuring901@gmail.com>
2024-03-04 15:43:31 +0100

    Implementirane metode u klasi Magacin

--- src/proizvodi/Magacin.java
+++ src/proizvodi/Magacin.java
@@ -1,27 +1,46 @@
 package proizvodi;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import interfejs.SpecifikacijaInterfejs;
 
 public class Magacin implements SpecifikacijaInterfejs {
 
+	List<Artikal> artikli = new ArrayList<>();
+	
 	@Override
 	public void dodajArtikal(Artikal a) {
-		// TODO Auto-generated method stub
+		if(a==null || artikli.contains(a) ) {
+			throw new IllegalArgumentException();
+		}
+		
+		artikli.add(a);
 		
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal a) {
-		// TODO Auto-generated method stub
+		if(a==null || !artikli.contains(a) ) {
+			throw new IllegalArgumentException();
+		}
+		
+		artikli.remove(a);
 		
 	}
 
 	@Override
 	public List<Artikal> vratiArtikle(int sifra) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Artikal> rezultat = new ArrayList<>();
+		
+		for(Artikal a: artikli) {
+			if(a.getSifra() == sifra) {
+				rezultat.add(a);
+			}
+		}
+		
+		return rezultat;
 	}
 
 }
