1584b027cec451ca28c42405cd68f1a299ad451b
doktoree <tjuring901@gmail.com>
2024-03-04 15:39:35 +0100

    Implementirane metode iz interfejsa

--- src/proizvodi/Magacin.java
+++ src/proizvodi/Magacin.java
@@ -1,27 +1,30 @@
 package proizvodi;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import interfejs.SpecifikacijaInterfejs;
 
 public class Magacin implements SpecifikacijaInterfejs {
 
+	List<Artikal> artikli = new ArrayList<Artikal>();
+	
 	@Override
 	public void dodajArtikal(Artikal a) {
-		// TODO Auto-generated method stub
+		artikli.add(a);
 		
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal a) {
-		// TODO Auto-generated method stub
+		artikli.remove(a);
 		
 	}
 
 	@Override
 	public List<Artikal> vratiArtikle(int sifra) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return artikli;
 	}
 
 }
